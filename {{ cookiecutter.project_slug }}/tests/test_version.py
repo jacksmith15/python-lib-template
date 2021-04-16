75131c88cd5a219e6835ef44f3b9f8fe5fea6e3d
Jack Smith <jack.je.smith@gmail.com>
2021-04-16 16:21:39 +0100

    Fix default test

--- {{ cookiecutter.project_slug }}/tests/test_version.py
+++ {{ cookiecutter.project_slug }}/tests/test_version.py
@@ -5,13 +5,12 @@ import changelog
 import {{ cookiecutter.package_name }} as package
 
 
-def test_version_matches_pyproject():
+def test_version_matches_pyproject() -> None:
     with open("pyproject.toml", "r") as file:
         pyproject = toml.loads(file.read())
     assert pyproject["tool"]["poetry"]["version"] == package.__version__
 
 
-@pytest.mark.xfail(strict=True, reason="No release has yet been made")
-def test_version_matches_changelog():
+def test_version_matches_changelog() -> None:
     log = changelog.load_from_file("CHANGELOG.md")
-    assert log.latest_version == package.__version__
+    assert log.latest_tag == package.__version__
